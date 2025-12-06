de8df372f0bbe552c9a34717bad7dfa35fd365d6
Axel Anderson <axel@ftml.net>
2025-12-06 14:44:02 -0500

    feat: migrate utilities to TypeScript

---
+++ frontend/src/utils/entropy.ts
@@ -0,0 +1,42 @@
+import type { Entry, Interval } from '../types/entry'
+import { getCurrentDate, getDate } from './date'
+
+const MAX = 7
+const MILLIS_IN_DAY = 24 * 60 * 60 * 1000
+
+// TODO: make user configurable
+const opts = {
+  entropy: 5,
+}
+
+export const getAvailableAtPlusEntropy = ({
+  dismissed_at,
+  interval,
+  duration,
+}: Pick<Entry, 'dismissed_at' | 'interval' | 'duration'>): {
+  availableAt: dayjs.Dayjs
+  diff: number
+} => {
+  const now = getCurrentDate()
+  const { entropy } = opts
+
+  const availableAt = dismissed_at
+    ? getDate(dismissed_at).add(duration, interval as any)
+    : now.subtract(1, 'seconds')
+
+  const diff = availableAt.diff(now)
+
+  if (entropy && diff > MILLIS_IN_DAY) {
+    const availableAtPlusEntropy = availableAt.add(
+      Math.floor(Math.random() * ((entropy / 10) * MAX)),
+      'days'
+    )
+
+    return {
+      availableAt: availableAtPlusEntropy,
+      diff: availableAtPlusEntropy.diff(now),
+    }
+  }
+
+  return { availableAt, diff }
+}
